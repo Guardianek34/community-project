7eeb86a4ebb00e62ab21e555fdf0692adfc21fcd
Guardianek <guardianek34@gmail.com>
2022-10-18 14:31:29 +0200

    Added initial commit

---
+++ src/test/java/com/example/communitysystem/CommunitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communitysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
